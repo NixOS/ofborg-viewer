2819e8382b81fb52bb07ad85d539246c43b7ba0d
Samuel Dionne-Riel <samuel@dionne-riel.com>
2018-01-20 12:59:03 -0500

    Separates App/Gui concerns.

---
+++ src/gui.js
@@ -0,0 +1,85 @@
+import html from "./lib/html";
+import bsod from "./lib/bsod";
+
+/**
+ * The line-oriented GUI for the application.
+ *
+ * The backlog can be filled with 
+ */
+class Gui {
+	constructor() {
+		console.log("Creating log interface....");
+		this.$app = null;
+		this.$log = null;
+		this.$backlog = null;
+
+		this.setFollowing(true);
+
+		this.$app = window.document.querySelectorAll("#ofborg-logviewer .app")[0];
+		if (!this.$app) {
+			return bsod("Couldn't hook app.");
+		}
+
+		this.$backlog = html(`<div class="backlog logger"></div>`)[0];
+		this.$log = html(`<div class="logger"></div>`)[0];
+		// Empties app...
+		this.$app.innerHTML = "";
+
+		// Appends the "app parts"
+		this.$app.appendChild(this.$backlog);
+		this.$app.appendChild(this.$log);
+
+		// Hooks on scroll
+		window.addEventListener("scroll", () => this.watchScroll())
+		console.log("... log interface created.")
+	}
+
+	setFollowing(following) {
+		if (following !== this.following) {
+			this.following = following;
+
+			const body = window.document.body;
+			if (following) {
+				body.classList.add("following");
+				body.classList.remove("not-following");
+			}
+			else {
+				body.classList.add("not-following");
+				body.classList.remove("following");
+			}
+		}
+	}
+
+	/**
+	 * Logs to the console.
+	 *
+	 * This can receive a class for some more styling.
+	 */
+	log(text, cls = "") {
+		const el = html(`<div class="${cls}"></div>`)[0];
+		el.innerText = text;
+		this.$log.appendChild(el);
+
+		const body = window.document.body;
+
+		if (this.following) {
+			body.scrollTop = body.scrollHeight;
+		}
+	}
+
+	/**
+	 * Marks the window as auto-scrollable or not.
+	 */
+	watchScroll() {
+		const body = window.document.body;
+		const scroll_bottom = Math.round(body.scrollTop) + Math.round(window.innerHeight);
+		const total_height = body.scrollHeight;
+
+		// Some fudging around because of higher and fractional DPI issues.
+		// On 1.5 DPI chrome, it is possible to get the scroll to bottom
+		// not matching with the total height *some* times.
+		this.setFollowing(Math.abs(total_height - scroll_bottom) < 5);
+	}
+}
+
+export default Gui;
--- src/index.js
+++ src/index.js
@@ -1,45 +1,30 @@
 import queryString from "query-string";
-import html from "./lib/html";
 import ready from "./lib/ready";
-import bsod from "./lib/bsod";
 import Listener from "./listener";
+import Gui from "./gui";
 
 /**
  * The logger app.
  */
 class App {
 	constructor() {
-		this.$app = null;
-		this.$log = null;
-		this.$backlog = null;
-
 		// Only "boot" the app when the DOM is ready.
 		ready(() => this.boot())
 	}
 
+	/**
+	 * Hooks and starts the app.
+	 *
+	 * This means:
+	 *   * Starts the GUI.
+	 *   * Reads parameters.
+	 *   * Starts the Listener.
+	 */
 	boot() {
-		this.setFollowing(true);
-
 		window.document.title = "Log viewer starting...";
-		this.$app = window.document.querySelectorAll("#ofborg-logviewer .app")[0];
-		if (!this.$app) {
-			return bsod("Couldn't hook app.");
-		}
-		console.log("Creating log interface....");
-		this.$backlog = html(`<div class="backlog"></div>`)[0];
-		this.$log = html(`<div class="logger"></div>`)[0];
+		this.gui = new Gui();
 		this.log("→ logger starting", "ofborg");
-		// Empties app...
-		this.$app.innerHTML = "";
-
-		// Appends the "app parts"
-		this.$app.appendChild(this.$backlog);
-		this.$app.appendChild(this.$log);
-		console.log("... log interface created.")
 		window.document.title = "Log viewer started...";
-
-		// Hooks on scroll
-		window.addEventListener("scroll", () => this.watchScroll())
 
 		// Loading parameters
 		const params = queryString.parse(location.search);
@@ -48,6 +33,7 @@ class App {
 			return;
 		}
 
+		// This will allow some app parts to log more information.
 		if (params["debug"]) {
 			window.DEBUG = true;
 		}
@@ -62,51 +48,13 @@ class App {
 		});
 	}
 
-	setFollowing(following) {
-		if (following !== this.following) {
-			this.following = following;
-
-			const body = window.document.body;
-			if (following) {
-				body.classList.add("following");
-				body.classList.remove("not-following");
-			}
-			else {
-				body.classList.add("not-following");
-				body.classList.remove("following");
-			}
-		}
-	}
-
 	/**
 	 * Logs to the console.
 	 *
 	 * This can receive a class for some more styling.
 	 */
 	log(text, cls = "") {
-		const el = html(`<div class="${cls}"></div>`)[0];
-		el.innerText = text;
-		this.$log.appendChild(el);
-
-		const body = window.document.body;
-
-		if (this.following) {
-			body.scrollTop = body.scrollHeight;
-		}
-	}
-
-	/**
-	 * Marks the window as auto-scrollable or not.
-	 */
-	watchScroll() {
-		const body = window.document.body;
-		const scroll_bottom = Math.round(body.scrollTop) + Math.round(window.innerHeight);
-		const total_height = body.scrollHeight;
-
-		// Some fudging around because of higher and fractional DPI issues.
-		// On 1.5 DPI chrome, it is possible to get the scroll to bottom
-		// not matching with the total height *some* times.
-		this.setFollowing(Math.abs(total_height - scroll_bottom) < 5);
+		this.gui.log(text, cls);
 	}
 }
 
