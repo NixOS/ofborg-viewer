2098671741eb6bdc5c1bbb750dad324ab5d4598f
Samuel Dionne-Riel <samuel@dionne-riel.com>
2018-01-21 21:37:45 -0500

    Adds a "fetch compat" thing that will error out when fetch is missing.

--- src/index.js
+++ src/index.js
@@ -7,5 +7,33 @@ import App from "./app";
  * Then, start the app.
  */
 
+// Fetch compat.
+{
+	const FETCH_MISSING = "fetch is required for this app to work properly.";
+
+	/**
+	 * Acts mostly like a promise.
+	 */
+	const pseudo_promise = function() {
+		return {
+			then: () => pseudo_promise(),
+			catch: (fn) => fn(new Error(FETCH_MISSING)),
+		};
+	};
+
+	/**
+	 * Replaces fetch when fetch is missing.
+	 */
+	const pseudo_fetch = function() { // eslint-disable-line
+		return pseudo_promise();
+	};
+
+	// Ensures calls to `fetch` don't crash the app.
+	if (!window.fetch) {
+		console.warn(FETCH_MISSING); // eslint-disable-line
+		window.fetch = pseudo_fetch; // eslint-disable-line
+	}
+}
+
 // Starts the app.
 window.APP = new App();
