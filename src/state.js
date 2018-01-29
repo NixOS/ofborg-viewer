7f62ef7b18515f77fc427b9308df2f4f7cbb8ae6
Samuel Dionne-Riel <samuel@dionne-riel.com>
2018-01-28 23:05:08 -0500

    Adds binding "the other way" with URL state.
    Back and forward now should work.
    
    For #1
    For #4

--- src/state.js
+++ src/state.js
@@ -1,4 +1,5 @@
 import queryString from "query-string";
+import isEqual from "lodash/isEqual";
 
 /**
  * Maps the state of the application to the history API.
@@ -18,9 +19,22 @@ class State {
 		const {history} = window;
 		// Loads from params in URL, then history in order of importance.
 		this.params = Object.assign({}, params, history.state);
+
+		window.onpopstate = (e) => this.handle_popstate(e);
 	}
 
-	set_state(new_state) {
+	handle_popstate(e) {
+		console.log(e);
+		const {state} = e;
+		if (state) {
+			this.set_state(state, {push: false});
+			if (this.on_state_change) {
+				this.on_state_change(state);
+			}
+		}
+	}
+
+	set_state(new_state, {push} = {push: true}) {
 		const {history} = window;
 		const params = Object.assign({}, this.params, new_state);
 		Object.keys(params).forEach((k) => {
@@ -29,8 +43,16 @@ class State {
 			}
 		});
 
+		if (isEqual(params, this.params)) {
+			// set_state won't fire on "identity" change.
+			return;
+		}
+
 		this.params = params;
-		history.pushState(this.params, "", `?${queryString.stringify(params)}`);
+
+		if (push) {
+			history.pushState(this.params, "", `?${queryString.stringify(params)}`);
+		}
 	}
 }
 
