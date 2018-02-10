13ee525b917b47105fd0fb23d5d9137daa237851
Samuel Dionne-Riel <samuel@dionne-riel.com>
2018-02-10 18:39:23 -0500

    Oopsie.

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation rec {
-	name = "qidigo-env";
+	name = "ofborg-logviewer-env";
 	# Makes those dependencies available in the environment.
 	buildInputs = [
 		nodejs-6_x           # Lock to 6.x
