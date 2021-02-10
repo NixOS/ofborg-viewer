4816c247aed7ba8f617d29603b67423e70ca5b48
Graham Christensen <graham@grahamc.com>
2021-02-09 21:58:42 -0500

    Update the URL for webstomp

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 const infra = "nix.ci";
 
 const WELL_KNOWN = `https://logs.${infra}/logs`;
-const SOCK = `wss://events.${infra}:15671/ws`;
+const SOCK = `wss://devoted-teal-duck.rmq.cloudamqp.com/ws/`;
 const SOCK_VHOST = `ofborg`;
-const AUTH = "logstream";
+const AUTH = "logviewer";
 const MAX_LINES = 25000;
 
 export {SOCK, SOCK_VHOST, AUTH, MAX_LINES, WELL_KNOWN};
